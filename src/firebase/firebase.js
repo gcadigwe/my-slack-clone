d3dbead0a24d6f14885afa5bc607a6c91ed4a6f5
GCAdigwe <gcadigwe@gmail.com>
2021-03-04 10:44:26 +0000

    initial firebase on the app

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -10,3 +10,12 @@ const firebaseConfig = {
   appId: "1:57818883481:web:7a22b66907e16ba08c1f08",
   measurementId: "G-XKGZKBYX2N",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+
+export default db;
